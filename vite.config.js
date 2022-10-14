61a3adda6c9495512e4b57d23d9adaee17cfb299
”Marie <mari576p@stud.kea.dk>
2022-10-14 08:28:31 +0200

    hej

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,29 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        billetter: resolve(__dirname, "src/billetter.html"),
+        detlevenderum: resolve(__dirname, "src/detlevenderum.html"),
+        events: resolve(__dirname, "src/events.html"),
+        frivillig: resolve(__dirname, "src/frivillig.html"),
+        frygtenforikkeatleve: resolve(
+          __dirname,
+          "src/frygtenforikkeatleve.html"
+        ),
+        gogoyoyoku: resolve(__dirname, "src/gogoyoyoku.html"),
+        mandliggersommandharredt: resolve(
+          __dirname,
+          "src/mandliggersommandharredt.html"
+        ),
+        omvildskud: resolve(__dirname, "src/omvildskud.html"),
+        program: resolve(__dirname, "src/program.html"),
+        rebirth: resolve(__dirname, "src/rebirth.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
+        vaermed: resolve(__dirname, "src/vaermed.html"),
+        vildmedmig: resolve(__dirname, "src/vildmedmig.html"),
+        whatfirestogetherwirestogether: resolve(
+          __dirname,
+          "src/whatfirestogetherwirestogether.html"
+        ),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
